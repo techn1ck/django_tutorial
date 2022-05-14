59ceac1a7010645229163ce2254b8a18dec47887
Nikita Yakovlev <nikita.uventa@gmail.com>
2022-05-14 15:40:55 +0300

    Part 7. Customizing admin panel

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from tkinter import CASCADE
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -12,6 +13,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
